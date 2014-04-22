aa367524b67f34876ac032e2a48ea3cca8d59163
Jo Geraerts <jo@umask.net>
2014-04-22 22:41:14 +0200

    s3 storage

---
+++ src/net/umask/imageresizer/s3store.clj
@@ -0,0 +1,13 @@
+(ns net.umask.imageresizer.s3store
+  (:require [aws.sdk.s3 :as s3])
+  (:use net.umask.imageresizer.store))
+
+(defrecord S3Store [bucket cred]
+  Store
+  (store-write [this name stream]
+    (s3/put-object (:cret this) (:bucket this) name stream))
+  (store-read [this name]
+    (s3/get-object (:cred this) (:bucket this) name)))
+
+(defn create-s3store [bucket cred]
+  (->S3Store bucket cred))
