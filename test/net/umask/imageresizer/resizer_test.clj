ab131b70375bfa15b5b3f452ac19225ff8bc20bd
Jo Geraerts <jo@umask.net>
2014-05-10 00:00:27 +0200

    rename tests

---
+++ test/net/umask/imageresizer/resizer_test.clj
@@ -0,0 +1,21 @@
+(ns net.umask.imageresizer.resizer_test
+  (:use net.umask.imageresizer.resizer
+        clojure.test
+        ring.mock.request))
+
+(def wrapped-echo (wrap-url-parser identity))
+
+(deftest urlparsing
+  (testing "test url parsing"
+    (are [x y] (= x (:imageresizer  (wrapped-echo (request :get  y))))
+         {:original "filename.jpg"
+          :size {:width 300
+                 :height 400 }
+          :checksum "deadbeef"} "/deadbeef/size/300x400/filename.jpg"
+         {:original "foo/bar/original.jpg"
+          :size {:size 300}
+          :checksum "deadbeef"} "/deadbeef/size/300/foo/bar/original.jpg"
+         {:original "foo/bar/original.jpg"
+          :size {:size 300}
+          :rotate {:angle  30}
+          :checksum "deadbeef"} "/deadbeef/rotate/30/size/300/foo/bar/original.jpg")))
