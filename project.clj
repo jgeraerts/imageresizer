7e94dbd2d579f9172cb5165f38fe947286319735
Jo Geraerts <jo@umask.net>
2014-07-28 23:01:17 +0200

    prepare 0.5.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.umask/imageresizer "0.5.0-SNAPSHOT"
+(defproject net.umask/imageresizer "0.5.0"
   :description  "simple on the fly image resizing server"
   :url "https://github.com/jogeraerts/imageserver"
   :license {:name "Eclipse Public License"
