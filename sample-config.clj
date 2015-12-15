c15fd34afcd6bb35a1ada25f150f6842a70468d7
Jo Geraerts <jo@umask.net>
2015-12-16 00:33:44 +0100

    add watermark sample

--- sample-config.clj
+++ sample-config.clj
@@ -2,19 +2,20 @@
   :http {:port 8080}
   :vhosts [["localhost"
             "127.0.0.1"] (resizer :secret "verysecret"
-                                              :source (s3source :bucket "bucketname"
-                                                                :cred {:endpoint "<endpoint goes here>"
-                                                                       ; example https://s3-eu-west-1.amazonaws.com  
-                                                                       :access-key "<access key goes here>"
-                                                                       :secret-key "<secret key goes here>"})
-                                              :cache (filecache :basedir "/var/lib/imgcache"))
-            ["some.other.domain.tld"
-             "some.alias.domain.tld"] (resizer :secret "alsoverysecret"
-                                               :source (httpsource :url "http://u.r.l/basepath/"
-                                                                   :rewrite {:fromregex #"^([0-9a-zA-Z]{160})\.jpg$"
-                                                                             :replacement "/medias/ignore.jpg?context=$1"})
+                                  :watermarks (filesource :basedir "/path/to/my/watermarks")
+                                  :source (s3source :bucket "bucketname"
+                                                    :cred {:endpoint "<endpoint goes here>"
+                                        ; example https://s3-eu-west-1.amazonaws.com  
+                                                           :access-key "<access key goes here>"
+                                                           :secret-key "<secret key goes here>"})
+                                  :cache (filecache :basedir "/var/lib/imgcache"))
+           ["some.other.domain.tld"
+            "some.alias.domain.tld"] (resizer :secret "alsoverysecret"
+                                              :source (httpsource :url "http://u.r.l/basepath/"
+                                                                  :rewrite {:fromregex #"^([0-9a-zA-Z]{160})\.jpg$"
+                                                                            :replacement "/medias/ignore.jpg?context=$1"})
                                                :cache (s3cache :bucket "cache"
                                                                :cred {:endpoint "<endpoint goes here>"
-                                                                       ; example https://s3-eu-west-1.amazonaws.com  
-                                                                       :access-key "<access key goes here>"
-                                                                       :secret-key "<secret key goes here>"}))])
+                                        ; example https://s3-eu-west-1.amazonaws.com  
+                                                                      :access-key "<access key goes here>"
+                                                                      :secret-key "<secret key goes here>"}))])
