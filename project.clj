829ea1644e5a0f31993a42feeaa6f9a1829b5d49
Jo Geraerts <jo@umask.net>
2016-03-14 12:49:19 +0100

    bump dependency versions
    * logback-classic
    * slf4j
    * jodatime

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jogeraerts/imageserver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ch.qos.logback/logback-classic "1.1.3"]
+  :dependencies [[ch.qos.logback/logback-classic "1.1.6"]
                  [clj-aws-s3 "0.3.10" :exclusions [commons-logging joda-time]]
                  [com.mortennobel/java-image-scaling "0.8.6"]
                  [com.stuartsierra/component "0.3.1"]
@@ -11,13 +11,13 @@
                  [com.twelvemonkeys.imageio/imageio-tiff "3.2.1"]
                  [digest "1.4.4"]
                  [http-kit "2.1.19"]
-                 [joda-time/joda-time "2.9.1"]
+                 [joda-time/joda-time "2.9.2"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.imgscalr/imgscalr-lib "4.2"]
-                 [org.slf4j/jcl-over-slf4j "1.7.13"]
+                 [org.slf4j/jcl-over-slf4j "1.7.18"]
                  [ring "1.4.0"]
                  [slingshot "0.12.2"]]
   :exclusions [commons-logging/commons-logging]
