82cc9a534035035ef4c181ddeeda4a33f02d6056
Jo Geraerts <jo.g@osudio.com>
2017-11-29 12:04:20 +0100

    prepare for next development iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.umask/imageresizer "0.7.2"
+(defproject net.umask/imageresizer "0.7.3-SNAPSHOT"
   :description  "simple on the fly image resizing server"
   :url "https://github.com/jogeraerts/imageserver"
   :license {:name "Eclipse Public License"
